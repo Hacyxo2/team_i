b7d7e26be33b65987fe4fc871ef64aa3808fcb8e
Hacyxo2 <83963272+Hacyxo2@users.noreply.github.com>
2021-09-15 18:00:56 +0900

    예시

--- team_i/src/team_i/MainFrame.java
+++ team_i/src/team_i/MainFrame.java
@@ -13,12 +13,13 @@ public class MainFrame extends JFrame {
 	{
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setTitle("Play Car");
-		setSize(1200, 600);
+		setSize(1000, 600);
 		setPreferredSize(getPreferredSize());
 		setVisible(true);
-		
+		System.out.println("test");
 		add(new Screen());
-		
+		//현재 commit을 한 상태라 아래 push Head 상태인데
+		//이렇게 설정하고 저장을 한다음
 		validate();
 	}
 }
