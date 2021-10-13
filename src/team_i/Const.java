c31cdb27b38e68f76ba53d96c9b405ccb8224512
Hacyxo2 <sh010200@naver.com>
2021-10-13 17:39:31 +0900

    Const.java gamePan_W = 800 -> 1000, gamePan_H = 600 -> 800

--- src/team_i/Const.java
+++ src/team_i/Const.java
@@ -2,6 +2,6 @@ package team_i;
 
 //https://beautifulkim.tistory.com/215
 public class Const {
-	public static final int gamePan_W = 800;
-	public static final int gamePan_H = 600;
+	public static final int gamePan_W = 1000;
+	public static final int gamePan_H = 800;
 }
